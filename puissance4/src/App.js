bb27bf5994ac21f730a4618dc6bbd015e47ab38a
valentin <valentin.loiseau@mbpdigital.com>
2019-06-11 13:32:19 +0200

    css

--- puissance4/src/App.js
+++ puissance4/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Puissance4 from './Components/Puissance4/Puissance4.jsx';
+import './App.css';
 
 export default class App extends Component {
   render() {
